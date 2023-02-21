675db8052dc4de0e337328b3f2129e68d6f185c5
larssantura <larssantura@gmail.com>
2023-02-21 21:01:55 +0200

    module removed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Menu from "/public/Icons/menu.svg";
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
 const Home = () => {
   return (
-    <div className="w-full">
+    <div>
       <Navbar />
       <SearchBar />
     </div>

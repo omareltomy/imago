40ea4731017d0b5d1f1c0533ae3ae81333ded74e
larssantura <larssantura@gmail.com>
2023-02-22 11:07:36 +0200

    search bar icon added and outline removed

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const SearchBar = () => {
@@ -8,11 +9,16 @@ const SearchBar = () => {
   };
   return (
     <div className="w-[100vw] h-[30vh] bg-[#F4F3F0] flex items-center justify-center">
-      <input
-        type="text"
-        className="w-[95vw] h-[3rem] bg-white border border-1  border-[#48474D] px-[1rem] focus:border-0"
-        onKeyDown={fetchResults}
-      />
+      <div className="relative">
+        <input
+          type="text"
+          className="w-[95vw] h-[3rem] bg-white border border-1  border-[#48474D] px-[1rem] focus:outline-none text-xl"
+          onKeyDown={fetchResults}
+        />
+        <div className="absolute inset-y-0 right-5 flex items-center pl-3">
+          <Image src="/icons/x.svg" width={50} height={50} alt="menu Icon" />
+        </div>
+      </div>
     </div>
   );
 };

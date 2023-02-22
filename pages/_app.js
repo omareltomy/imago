4a17e2ecf83507b928972ea01b04baa9ae34cfdb
larssantura <larssantura@gmail.com>
2023-02-22 12:43:31 +0200

    title and fav icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>IMAGO</title>
+        <link rel="shortcut icon" href="/icons/logo.svg" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

17456c9301df962b7847bf9463d0e4a91711dabb
larssantura <larssantura@gmail.com>
2023-02-26 14:36:33 +0200

    useEffect issue fixed

--- pages/[id].js
+++ pages/[id].js
@@ -15,7 +15,7 @@ const Item = () => {
 
     setId(IdString);
     fetch(
-      `https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/media/${id}`
+      `https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/media/${IdString}`
     )
       .then((response) => response.json())
       .then((data) => {

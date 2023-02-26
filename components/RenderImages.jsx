531976ec602d17a85f10946a5749044ac203feef
larssantura <larssantura@gmail.com>
2023-02-26 15:22:38 +0200

    code cleaning and finishing up the requirements

--- components/RenderImages.jsx
+++ components/RenderImages.jsx
@@ -1,32 +1,12 @@
-// Import necessary dependencies
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import SearchBar from "./SearchBar";
 
-// Define the component
 const RenderImages = () => {
-  // Define state for the list of images
-  const [images, setImages] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
-  // Use the `useEffect` hook to fetch a random set of curated images when the component mounts
-  useEffect(() => {
-    // Choose a random page number between 1 and 50
-    const randomPage = Math.floor(Math.random() * 50) + 1;
 
-    // Fetch the list of curated images from the Unsplash API
-    fetch(
-      `https://api.unsplash.com/photos/?per_page=9&page=${randomPage}&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => setImages(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  // Define a function to handle search queries
-  // Define a function to handle search queries
   const handleSearchSubmit = (searchQuery) => {
-    // Fetch a list of images that match the search query from the Unsplash API
     fetch(
       `https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/search?query=${searchQuery}`
     )
@@ -35,7 +15,6 @@ const RenderImages = () => {
       .catch((error) => console.log(error));
   };
 
-  // Render the component
   return (
     <>
       {/* Render the search bar component */}
@@ -43,26 +22,8 @@ const RenderImages = () => {
 
       {/* Render the list of images in a responsive grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 py-8">
-        {images.length > 0 && searchResults.length === 0
-          ? images.map((image) => (
-              <Link
-                href={`/${image.id}`}
-                key={image.id}
-                passHref
-                legacyBehavior
-              >
-                <a className="block">
-                  <Image
-                    src={image.urls.regular}
-                    alt={image.alt_description}
-                    width={320}
-                    height={image.height * (320 / image.width)}
-                    className="w-full h-full object-cover"
-                  />
-                </a>
-              </Link>
-            ))
-          : searchResults.map((image) => (
+        {searchResults.length > 0
+          ? searchResults.map((image) => (
               <Link
                 href={`/${image["media-id"]}`}
                 key={image["media-id"]}
@@ -79,7 +40,8 @@ const RenderImages = () => {
                   />
                 </a>
               </Link>
-            ))}
+            ))
+          : null}
       </div>
     </>
   );
